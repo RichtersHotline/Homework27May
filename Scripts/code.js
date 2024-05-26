1748129308595d1f353e81f5429f2e31f9af1069
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-05-26 18:41:13 +0200

    done

--- Scripts/code.js
+++ Scripts/code.js
@@ -1,3 +1,27 @@
 // Exercise 1
 
+let UserInput = document.getElementById("PalChecker");
+let Paline = document.getElementById("Palin")
+
+function isPalindrome() {
+let Reverso = UserInput.value.split('').reverse().join('').toLowerCase();
+
+
+    if (Reverso == UserInput.value ) {
+
+        Paline.innerHTML = "That is a Palindrome! :D"
+console.log("That is a B")
+
+    }
+    
+    else {
+
+    Paline.innerHTML = "That is not a Palindrome :("
+
+    }
+    
+    console.log(Reverso)
+
+
+}
 
